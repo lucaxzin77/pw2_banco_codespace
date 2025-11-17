75d39076efbdb88d4e42f9330f98ed829790c3dd
Lucas Marques <lucaxzinmf069@gmail.com>
2025-11-17 11:34:50 +0000

    Service Finalizado!

--- web/service/ContaPoupanca.php
+++ web/service/ContaPoupanca.php
@@ -1,5 +1,41 @@
 <?php 
 
-class ContaPoupanca{
+require_once 'Conta.php';
+
+class ContaPoupanca extends Conta{
     
+    #[Override]
+    public function Depositar(float $valor): array{
+
+        if($valor <= 0)
+            throw new Exception(message: "O valor do depósito deve ser positivo.");
+        
+        $this->saldo += $valor * 0.49;
+
+        return [
+            "tipo" => "deposito",
+            "valor_depositado" => $valor,
+            "saldo_atual" => $this->saldo,
+            "taxa" => $valor * 0.005
+        ];
+    }
+
+    #[Override]
+    public function Sacar(float $valor): array{
+        
+        if($valor <= 0)
+            throw new Exception(message: "O valor do saque deve ser positivo.");
+        
+        if($valor > $this->saldo)
+            throw new Exception(message: "Saldo insuficiente para o saque!");
+
+        $this->saldo -= $valor * 1.005;
+
+        return [
+            "tipo" => "saque",
+            "valor_sacado" => $valor,
+            "saldo_atual" => $this->saldo,
+            "taxa" => $valor * 0.005
+        ];
+    }
 }
--- web/service/Titular.php
+++ web/service/Titular.php
@@ -1,5 +1,58 @@
 <?php 
 
+require_once 'Conta.php';
+
 class Titular{
-    
+    protected string $nome,
+    protected string $cpf,
+    protected string $endereco
+
+    public Titular($nome, $cpf, $endereco){
+        $this->nome = $nome,
+        $this->cpf = $cpf,
+        $this->endereco = $endereco
+    }
+
+    public setNome($nome){
+        if(strlen(trim($nome)) < 3){
+            throw new InvalidArgumentException("O nome do titular deve ter pelo menos 3 caracteres!")
+        }
+
+        if(preg_match('/\d/', $nome)){
+            throw new InvalidArgumentException("O nome do titular não pode conter números!")
+        }
+
+        $this->nome = $nome;
+    }
+
+    public setCpf($cpf){
+        if(strlen(trim($cpf)) != 11){
+            throw new InvalidArgumentException("O CPF deve conter exatamente 11 dígitos numéricos!");
+        }
+
+        $this->cpf = $cpf;
+    }
+
+    public setEndereco($endereco){
+        if(is_null($endereco))
+        {
+            throw new InvalidArgumentException("O endereço não pode ser vazio!");
+        }
+        $this->endereco = $endereco;
+    }
+
+    public getNome():string{
+        return $this->nome;
+    }
+    public getCpf():string{
+        return $this->cpf;
+    }
+    public getEndereco():string{
+        return $this->endereco;
+    }
+
+    #[Override]
+    public ToString():string{
+        return 'Nome: '.''$this->nome''.', CPF: '.''$this->cpf''.', Endereço: '.''$this->endereco''.'';
+    }
 }
